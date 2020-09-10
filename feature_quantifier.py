59c8a37c3d9cee1a1e43e8054d559ce0467f778c
Christian Schudoma <christian.schudoma@embl.de>
2020-09-10 17:50:16 +0200

    moved overlap functionality to gff_dbm, counting to overlap_counter

--- feature_quantifier.py
+++ feature_quantifier.py
@@ -1,7 +1,6 @@
 import os
 import time
 import sys
-import gzip
 from collections import Counter
 import json
 import yaml
@@ -24,7 +23,7 @@ class OverlapCounter(dict):
 		self.seqcounts = Counter()
 		self.featcounts = dict()
 		pass
-	def update_unique_counts(self, rid, overlaps, n_aln=1):
+	def update_unique_counts(self, rid, overlaps):
 		'''
 		Generates a hit list from the overlaps resulting from an intervaltree query,
 		adds the number of alternative alignments and stores the results for each reference sequence.
@@ -32,7 +31,7 @@ class OverlapCounter(dict):
 		self.setdefault(rid, Counter()).update((ovl.begin, ovl.end) for ovl in overlaps)
 		self.seqcounts[rid] += 1
 
-	def annotate_unique_counts(self, bam, gff_source):
+	def annotate_unique_counts(self, bam, gff_dbm):
 		'''
 		Look up and collate functional annotation for unique hits
 		'''
@@ -40,13 +39,14 @@ class OverlapCounter(dict):
 		t0 = time.time()
 		for rid, intervals in self.items():
 			ref, _ = bam.get_reference(rid)
-			gff_annotation = gff_source.read_gff_data(ref, include_payload=True)
 			for region, count in intervals.items():
-				region_annotation = gff_annotation.get((ref,) + region, dict())
+				region_annotation = gff_dbm.get_data(ref, *region)
 				for ftype, values in region_annotation.items():
 					if ftype != "ID":
 						for v in values:
 							self.featcounts.setdefault(ftype, dict()).setdefault(v, [0, region[1] - region[0] + 1])[0] += count
+			del self[rid]
+		self.clear()
 		t1 = time.time()
 		print("Processed unique counts in {n_seconds}s.".format(n_seconds=t1-t0), flush=True)
 	def dump_counts(self, bam):
@@ -61,30 +61,24 @@ class OverlapCounter(dict):
 				for subf, (subf_count, f_len) in sorted(counts.items()):
 					print(subf, "{:.5f}\t{:.5f}\t{:.5f}\t{:.5f}".format(*OverlapCounter.normalise_counts(subf_count, f_len)), flush=True, sep="\t", file=feat_out)
 
-
-class FeatureQuantifier:
-	def _read_count_config(self, config):
-		default = {"multiple": self.default_multiple_alignments, "normalization": self.default_normalization}
-		self._count_config = yaml.load(open(config), Loader=yaml.SafeLoader) if config is not None else default
-	def read_gff_index(self, gff_index):
-		self.gff_index = dict()
-		for line in open(gff_index, "rt"):
+class GffDatabaseManager:
+	def _read_index(self, f):
+		db_index = dict()
+		for line in open(f, "rt"):
 			line = line.strip().split("\t")
-			self.gff_index.setdefault(line[0], list()).append(list(map(int, line[1:3])))
-	def __init__(self, gff_db, gff_index, count_config=None, multiple_alignments="unique_only", normalization="scaled", gff_gzipped=True):
-		self.gff_db = gff_db
-		self.default_multiple_alignments = multiple_alignments
-		self.default_normalization = normalization
-		gff_open = gzip.open if gff_gzipped else open
-		self.gff_data = gff_open(gff_db, "rt")
-
-		self.read_gff_index(gff_index)
-		self._read_count_config(count_config)
-	def read_gff_data(self, ref_id, include_payload=False):
+			db_index.setdefault(line[0], list()).append(list(map(int, line[1:3])))
+		return db_index
+	def __init__(self, db, db_index):
+		self.db_index = self._read_index(db_index)
+		self.db = open(db, "rt")
+		self.loaded_data = None
+		self.loaded_ref = None
+		self.interval_tree = None
+	def _read_data(self, ref_id, include_payload=False):
 		gff_annotation = dict()
-		for offset, size in self.gff_index.get(ref_id, list()):
-			self.gff_data.seek(offset)
-			for line in self.gff_data.read(size).strip("\n").split("\n"):
+		for offset, size in self.db_index.get(ref_id, list()):
+			self.db.seek(offset)
+			for line in self.db.read(size).strip("\n").split("\n"):
 				if not line.startswith("#"):
 					line = line.strip().split("\t")
 					features = None
@@ -95,93 +89,64 @@ class FeatureQuantifier:
 		if not gff_annotation and not include_payload:
 			print("WARNING: contig {contig} does not have an annotation in the index.".format(contig=ref_id), file=sys.stderr, flush=True)
 		return gff_annotation
+	def _load_data(self, ref, include_payload=False):
+		if self.loaded_ref != ref:
+			self.loaded_ref = ref
+			self.loaded_data = self._read_data(ref, include_payload=include_payload)
+			self.interval_tree = IntervalTree.from_tuples(sorted([key[1:] for key in self.loaded_data]))
 
-	def process_unique_cache(self, interval_tree, rid):
-		for qname, cached in self.umap_cache.items():
-			n_aln = len(cached)
+	def get_data(self, ref, start, end):
+		self._load_data(ref, include_payload=True)
+		return self.loaded_data.get((ref, start, end), dict())
+
+	def get_overlaps(self, ref, start, end):
+		self._load_data(ref)
+		return self.interval_tree[start:end]
+
+
+class FeatureQuantifier:
+	def _read_count_config(self, config):
+		default = {"multiple": self.default_multiple_alignments, "normalization": self.default_normalization}
+		self._count_config = yaml.load(open(config), Loader=yaml.SafeLoader) if config is not None else default
+
+	def __init__(self, gff_db, gff_index, count_config=None, multiple_alignments="unique_only", normalization="scaled"): #, gff_gzipped=True):
+		self.gff_dbm = GffDatabaseManager(gff_db, gff_index)
+		self.default_multiple_alignments = multiple_alignments
+		self.default_normalization = normalization
+		self._read_count_config(count_config)
+		self.umap_cache = dict()
+
+	def process_unique_cache(self, rid, ref):
+		for qname, uniq_aln in self.umap_cache.items():
+			n_aln = len(uniq_aln)
 			if n_aln > 2:
 				print("WARNING: more than two primary alignments for {qname} ({n}). Ignoring.".format(qname=qname, n=n_aln), file=sys.stderr, flush=True)
 				continue
 
-			start, end = cached[0][1:] if n_aln == 1 else BamFile.calculate_fragment_borders(*cached[0][1:], *cached[1][1:])
-			self.overlap_counter.update_counts(rid, interval_tree[start:end], n_aln=1)
+			start, end = uniq_aln[0][1:] if n_aln == 1 else BamFile.calculate_fragment_borders(*uniq_aln[0][1:], *uniq_aln[1][1:])
+			self.overlap_counter.update_unique_counts(rid, self.gff_dbm.get_overlaps(ref, start, end))
 
 		self.umap_cache.clear()
 
-	def update_reference_data(self, ref, rid, current_ref, interval_tree):
-		if current_ref is not None:
-			self.process_unique_cache(interval_tree, rid)
-		current_ref = ref
-		gff_annotation = self.read_gff_data(current_ref)
-		intervals = sorted([key[1:] for key in gff_annotation])
-		interval_tree = IntervalTree.from_tuples(intervals)
-
-		return rid, current_ref, interval_tree
-
-	def process_bam(self, bamfile, out_prefix):
-		bam = BamFile(bamfile)
-		# TODO multimappers
-		# multimappers = bam.get_multimappers()
-		multimappers = dict()
+	def process_unique_alignments(self, bam):
 		t0 = time.time()
-		self.umap_cache, self.mmap_cache = dict(), dict()
-		self.overlap_counter = OverlapCounter(out_prefix)
-
 		current_ref, current_rid = None, None
-		interval_tree = None
 
-		for aln_count, aln in bam.get_alignments(disallowed_flags=0x800):
-
+		for aln_count, aln in bam.get_alignments(allow_multiple=False, disallowed_flags=0x800):
 			ref = bam.get_reference(aln.rid)[0]
 			start, end = aln.start, aln.end
 			qname = aln.get_hash()
 
 			if ref != current_ref:
-				current_rid, current_ref, interval_tree = self.update_reference_data(
-					ref, aln.rid, current_ref, interval_tree)
 				print("{time}\tNew reference: {ref} ({rid}/{n_ref}). {n_aln} alignments processed.".format(
 					time=datetime.now().strftime("%m/%d/%Y,%H:%M:%S"),
-					ref=current_ref,
-					rid=current_rid,
-					n_ref=bam.n_references(),
-					n_aln=aln_count),
+					ref=ref, rid=aln.rid, n_ref=bam.n_references, n_aln=aln_count),
 					file=sys.stderr, flush=True)
 
+				self.process_unique_cache(current_rid, current_ref)
+				current_rid, current_ref = aln.rid, ref
 
-			# if not aln.is_primary() or aln.mapq == 0:
-			if aln.mapq == 0:
-				continue # TODO: re-enable multimappers
-				mm_count, primaries = multimappers.get(qname, [0, set()])
-				if not primaries:
-					# ignore secondary alignments without existing primary alignment for now
-					#print("WARNING: could not find primary alignments for {aln_qname}".format(aln_qname=qname), flush=True, file=sys.stderr)
-					continue
-				# we need to have the primary alignment flag in the key so we can process the primary alignment properly
-				aln_key = (aln.rid, aln.start, aln.end, aln.is_primary())
-				# special treatment for the secondary alignments
-				# this is stuff i've seen coming out of ngless/bwa
-				if not aln_key[-1]:
-					mm_count -= 1
-					seen_primaries = [k for k in self.mmap_cache.get(qname, set()) if k[-1]]
-					if not mm_count and len(seen_primaries) == len(primaries):
-						# all primary and secondary alignments of read have been seen
-						# TODO process alignments
-						self.mmap_cache.pop(qname)
-						continue
-					multimappers[qname][0] = mm_count
-
-					if aln_key[:-1] in primaries:
-						# there are sometimes secondary alignments that seem to be identical to the primary -> ignore
-						continue
-					if aln_key in self.mmap_cache.get(qname, set()):
-						# if r1 and r2 are identical and generate two separate individual alignments -> ignore
-						# i don't know why that would happen: very short fragment sequenced twice, i.e. from each direction or r1/r2 mislabeled/duplicated?
-						continue
-					
-				self.mmap_cache.setdefault(qname, set()).add(aln_key)
-				continue
-
-			elif aln.is_paired() and aln.rid == aln.rnext:
+			if aln.is_paired() and aln.rid == aln.rnext and not aln.flag & 0x8:
 				# need to keep track of read pairs to avoid dual counts
 				# check if the mate has already been seen
 				mates = self.umap_cache.setdefault(qname, list())
@@ -192,7 +157,6 @@ class FeatureQuantifier:
 						continue # i don't think this ever happens
 					else:
 						start, end = BamFile.calculate_fragment_borders(aln.start, aln.end, *mates[0][1:])
-					# self.umap_cache.pop(qname)
 					del self.umap_cache[qname]
 				else:
 					# otherwise cache the first encountered mate and advance to the next read
@@ -200,16 +164,47 @@ class FeatureQuantifier:
 					continue
 
 			# at this point only single-end reads and merged pairs should be processed here
-			self.overlap_counter.update_counts(aln.rid, interval_tree[start:end], n_aln=1)
+			self.overlap_counter.update_unique_counts(aln.rid, self.gff_dbm.get_overlaps(current_ref, start, end))
 
 		# clear cache
-		self.process_unique_cache(interval_tree, current_rid)
+		self.process_unique_cache(current_rid, current_ref)
 		t1 = time.time()
 
 		print("Processed {n_align} primary alignments in {n_seconds:.3f}s.".format(
 			n_align=aln_count, n_seconds=t1-t0), flush=True
 		)
 
-		self.overlap_counter.process_counts(bam, self)
+	def process_bam(self, bamfile, out_prefix):
+		self.overlap_counter = OverlapCounter(out_prefix)
+		bam = BamFile(bamfile)
+
+		self.process_unique_alignments(bam)
+		#bam.rewind()
+		#self.process_multiple_alignments(bam)
+
+		self.overlap_counter.annotate_unique_counts(bam, self.gff_dbm)
 		self.overlap_counter.dump_counts(bam)
 		print("Finished.", flush=True)
+
+	def process_multiple_alignments(self, bam):
+		multiples = list() #dict()
+		size, n_aln = 0, 0
+		t0 = time.time()
+		read_lut = dict()
+		for aln_count, aln in bam.get_alignments(allow_unique=False, disallowed_flags=0x800):
+			qname, v = aln.get_hash(), (aln.rid, aln.start, aln.end, aln.is_primary())
+			size += sys.getsizeof(qname) + sum(map(sys.getsizeof, v))
+			n_aln += 1
+			read_id = read_lut.setdefault(qname, len(read_lut))
+			#multiples.setdefault(aln.get_hash(), list()).append((aln.rid, aln.start, aln.end, aln.is_primary()))
+			multiples.append((read_id, aln.rid, aln.start, aln.end, aln.is_primary()))
+		size += sys.getsizeof(multiples)
+		t1 = time.time()
+		print("Processed {n_align} secondary alignments (size={size}b) in {n_seconds:.3f}s.".format(
+			n_align=n_aln, n_seconds=t1-t0, size=size), flush=True)
+
+		n_no_primary = 0
+		#for alignments in multiples: #.values():
+		#	if all(not is_primary for _, _, _, is_primary in alignments):
+		#		n_no_primary += 1
+		#print("{n_no_primary} secondary alignments don't have a primary alignment.".format(n_no_primary=n_no_primary), flush=True)
