34de8fd434d8ca7cf18cfe0541267629b53edfd6
Christian Schudoma <christian.schudoma@embl.de>
2020-09-11 13:16:36 +0200

    Made gffindex/gffquant output file handling a bit more user-friendly.

--- gffquant/__main__.py
+++ gffquant/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import pathlib
 
 from gffquant.feature_quantifier import FeatureQuantifier
 
@@ -7,11 +8,16 @@ def main():
 	ap = argparse.ArgumentParser()
 	ap.add_argument("gff_file", type=str)
 	ap.add_argument("bam_file", type=str)
-	ap.add_argument("out_prefix", type=str)
+	ap.add_argument("--out_prefix", "-o", type=str, default="gffquant")
 	ap.add_argument("--count_config", type=str)
 	args = ap.parse_args()
 
 	gff_index = args.gff_file + ".index"
+
+	if os.path.dirname(args.out_prefix):
+		pathlib.Path(os.path.dirname(args.out_prefix)).mkdir(exist_ok=True, parents=True)
+
+
 	if not os.path.exists(args.gff_file):
 		raise ValueError("gff database does not exist", args.gff_file)
 	if not os.path.exists(gff_index):
