2b47beb3c0a9cfbc4f7093eca9842b5a0b5430d3
Christian Schudoma <christian.schudoma@embl.de>
2020-10-14 23:26:52 +0200

    fixing issues

--- gffquant/feature_quantifier.py
+++ gffquant/feature_quantifier.py
@@ -9,6 +9,11 @@ from gffquant.bamreader import BamFile
 from gffquant.gff_dbm import GffDatabaseManager
 from gffquant.overlap_counter import OverlapCounter
 
+SUPPL_ALN_FLAG = 0x800
+WEIRD_MASK_FLAG = 0x1c0 # this masks everything except first/second in pair and secondary aln
+MATE_UNMAPPED_FLAG = 0x8
+FIRST_IN_PAIR_FLAG = 0x40
+SECOND_IN_PAIR_FLAG = 0x80
 
 class FeatureQuantifier:
 
@@ -36,26 +41,27 @@ class FeatureQuantifier:
 		current_ref, current_rid = None, None
 
 		with open(self.out_prefix + ".ambig_tmp.txt", "wt") as ambig_out_tmp:
-			for aln_count, aln in bam.get_alignments(allow_multiple=self.ambig_mode != "unique_only", allow_unique=True, disallowed_flags=0x800):
-				ref = bam.get_reference(aln.rid)[0]
+			for aln_count, aln in bam.get_alignments(allow_multiple=self.ambig_mode != "unique_only", allow_unique=True, disallowed_flags=SUPPL_ALN_FLAG):
 				start, end = aln.start, aln.end
 				qname = aln.qname
 
-				if ref != current_ref:
+				if aln.rid != current_rid:
+					if current_rid is not None:
+						self.process_unique_cache(current_rid, current_ref)
+					current_ref, current_rid = bam.get_reference(aln.rid)[0], aln.rid
+					
 					print("{time}\tNew reference: {ref} ({rid}/{n_ref}). {n_aln} alignments processed.".format(
 						time=datetime.now().strftime("%m/%d/%Y,%H:%M:%S"),
-						ref=ref, rid=aln.rid, n_ref=bam.n_references(), n_aln=aln_count),
+						ref=current_ref, rid=aln.rid, n_ref=bam.n_references(), n_aln=aln_count),
 						file=sys.stderr, flush=True)
-					self.process_unique_cache(current_rid, current_ref)
-					current_rid, current_ref = aln.rid, ref
 
 				if aln.is_ambiguous() and self.ambig_mode == "1overN":
-					overlaps = self.gff_dbm.get_overlaps(ref, start, end)
+					overlaps = self.gff_dbm.get_overlaps(current_ref, start, end)
 					if not overlaps:
-						print(aln.qname, aln_count, -1, -1, -1, aln.flag & 0x1c0, file=ambig_out_tmp, sep="\t")
+						print(aln.qname, aln_count, -1, -1, -1, aln.flag, file=ambig_out_tmp, sep="\t")
 					for ovl in overlaps:
-						print(aln.qname, aln_count, aln.rid, ovl.begin, ovl.end, aln.flag & 0x1c0, file=ambig_out_tmp, sep="\t")
-				elif aln.is_unique() and  aln.is_paired() and aln.rid == aln.rnext and not aln.flag & 0x8:
+						print(aln.qname, aln_count, aln.rid, ovl.begin, ovl.end, aln.flag, file=ambig_out_tmp, sep="\t")
+				elif aln.is_unique() and aln.is_paired() and aln.rid == aln.rnext and not aln.flag & MATE_UNMAPPED_FLAG:
 					# need to keep track of read pairs to avoid dual counts
 					# check if the mate has already been seen
 					mates = self.umap_cache.setdefault(qname, list())
@@ -82,6 +88,31 @@ class FeatureQuantifier:
 		    n_align=aln_count, n_seconds=t1-t0), flush=True
 		)
 
+	def _read_ambiguous_alignments(self, ambig_in):
+		read_ids, ambig_aln = dict(), list()
+		for qname, *read_data in csv.reader(ambig_in, delimiter="\t"):
+			qname = read_ids.setdefault(qname, len(read_ids))
+			ambig_aln.append((qname, ) + tuple(map(int, read_data)))
+
+		return ambig_aln
+
+	def _process_ambiguous_aln_groups(self, ambig_aln, bam):
+		n_align, current_group = 0, None
+		for aln in sorted(ambig_aln):
+			if current_group is None or current_group.qname != aln[0]:
+				if current_group:
+					n_align += current_group.n_align()
+					current_group.resolve(self.overlap_counter, self.gff_dbm, bam)
+				current_group = AmbiguousAlignmentGroup(aln)
+			else:
+				current_group.add_alignment(aln)
+
+		if current_group is not None:
+			n_align += current_group.n_align()
+			current_group.resolve(self.overlap_counter, self.gff_dbm, bam)
+
+		return n_align
+		
 
 	def process_data(self, bamfile):
 		bam = BamFile(bamfile)
@@ -96,28 +127,9 @@ class FeatureQuantifier:
 			# b'ST-K00119:83:HH22HBBXX:1:2203:9912:18107\x00' 4       126964  129574  2       0       256
 			t0 = time.time()
 			with open(self.out_prefix + ".ambig_tmp.txt") as ambig_in:
-				read_ids = dict()
-				ambig_aln = list()
-				for read, aln, rid, start, end, flag in csv.reader(ambig_in, delimiter="\t"):
-					read = read_ids.setdefault(read, len(read_ids))
-					ambig_aln.append((read, int(aln), int(rid), int(start), int(end), int(flag)))
-				read_ids.clear()
+				ambig_aln = self._read_ambiguous_alignments(ambig_in)
 
-			n_align = 0
-			current_group = None
-			for aln_count, aln in enumerate(sorted(ambig_aln)):
-				if not current_group or current_group.qname != aln[0]:
-					if current_group:
-						n_align += current_group.n_align()
-						current_group.resolve(self.overlap_counter, self.gff_dbm, bam)
-					current_group = AmbiguousAlignmentGroup(aln)
-				else:
-					current_group.add_alignment(aln)
-
-			if current_group is not None:
-				n_align += current_group.n_align()
-				current_group.resolve(self.overlap_counter, self.gff_dbm, bam)
-
+			n_align = self._process_ambiguous_aln_groups(ambig_aln, bam)
 			os.remove(self.out_prefix + ".ambig_tmp.txt")
 
 			t1 = time.time()
@@ -135,34 +147,32 @@ class AmbiguousAlignmentGroup:
 		self.secondaries = list()
 		self.primary1, self.primary2 = None, None
 		self.qname = aln[0]
-		self.uniq_aln = set()
+		self.uniq_read_ids = set()
 		self.unannotated = 0
 		self.add_alignment(aln)
 
-	# (read, int(aln), int(rid), int(start), int(end), int(flag1) | int(flag2))
-
+	# (read, int(aln), int(rid), int(start), int(end), int(flag))
+	# int_qname        8       -1      -1      -1      256
 	def add_alignment(self, aln):
-		short_aln = aln[1:5] #(aln.rid, aln.start, aln.end)
+		short_aln = aln[1:] #(aln.rid, aln.start, aln.end)
 		if aln[0] == -1:
 			self.unannotated += 1
-		elif aln[-1] & 0x40 and self.primary1 is None:
+		elif aln[-1] & FIRST_IN_PAIR_FLAG and self.primary1 is None:
 			self.primary1 = short_aln
-		elif aln[-1] & 0x8 and self.primary2 is None:
+		elif aln[-1] & SECOND_IN_PAIR_FLAG and self.primary2 is None:
 			self.primary2 = short_aln
 		else:
 			self.secondaries.append(short_aln)
-		self.uniq_aln.add(aln[0])
+		self.uniq_read_ids.add(aln[0])
 
 	def n_align(self):
-		return len(self.uniq_aln.difference({-1}))
+		return len(self.uniq_read_ids.difference({-1}))
 
 	def resolve(self, counter, gff_dbm, bam, distmode="all1"):
 		hits = dict()
 
 		alignments = {aln[1:] for aln in [self.primary1, self.primary2] + self.secondaries if aln is not None and aln[1] != -1}
-		for rid, start, end in alignments:
+		for rid, start, end, flag in alignments:
 			hits.setdefault(rid, set()).add((start, end))
 
-		n_aln = len(self.uniq_aln.difference({-1}))
-		unannotated = self.unannotated
-		counter.update_ambiguous_counts(hits, unannotated, n_aln, gff_dbm, bam, feat_distmode=distmode)
+		counter.update_ambiguous_counts(hits, self.unannotated, self.n_align(), gff_dbm, bam, feat_distmode=distmode)
