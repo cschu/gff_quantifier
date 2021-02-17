5c08d8768a3778a2a27dee09d9bceba853d20a37
Christian Schudoma <cschu1981@gmail.com>
2021-02-18 00:15:02 +0100

    addressing issue #4 * added check for empty ambig_dumpfile * version -> 1.0.1

--- setup.py
+++ setup.py
@@ -12,48 +12,48 @@ from gffquant import __version__ as gffquant_version
 here = path.abspath(path.dirname("__file__"))
 
 with open(path.join(here, "DESCRIPTION.md"), encoding="utf-8") as description:
-	description = long_description = description.read()
+    description = long_description = description.read()
 
-	name="gffquant"
-	version = gffquant_version
+    name="gffquant"
+    version = gffquant_version
 
-	if sys.version_info.major != 3:
-		raise EnvironmentError("""{toolname} is a python module that requires python3, and is not compatible with python2.""".format(toolname=name))
+    if sys.version_info.major != 3:
+        raise EnvironmentError("""{toolname} is a python module that requires python3, and is not compatible with python2.""".format(toolname=name))
 
-	setup(
-		name=name,
-		version=version,
-		description=description,
-		long_description=long_description,
-		url="https://github.com/cschu/gff_quantifier",
-		author="Christian Schudoma",
-		author_email="christian.schudoma@embl.de",
-		license="MIT",
-		classifiers=[
-			"Development Status :: 4 - Beta",
-			"Topic :: Scientific Engineering :: Bio/Informatics",
-			"License :: OSI Approved :: MIT License",
-	        "Operating System :: POSIX :: Linux",
-			"Programming Language :: Python :: 3.7"
-		],
-		zip_safe=False,
-		keywords="large reference data genomic feature quantification",
-		packages=find_packages(exclude=["test"]),
-		#scripts=["util/gff_indexer.py"],
-		install_requires=[
-			"intervaltree",
-			"numpy",
-			"pandas"
-		],
-		entry_points={
-			"console_scripts": [
-				"gffquant=gffquant.__main__:main",
-				"gffindex=gffquant.gff_indexer:main",
-				"collate_counts=gffquant.collate_counts:main"
-			],
-		},
-		scripts=["nextflow/gffquant.nf"],
-		package_data={},
-		include_package_data=True,
-		data_files=[],
-	)
+    setup(
+        name=name,
+        version=version,
+        description=description,
+        long_description=long_description,
+        url="https://github.com/cschu/gff_quantifier",
+        author="Christian Schudoma",
+        author_email="christian.schudoma@embl.de",
+        license="MIT",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Topic :: Scientific Engineering :: Bio/Informatics",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3.7"
+        ],
+        zip_safe=False,
+        keywords="large reference data genomic feature quantification",
+        packages=find_packages(exclude=["test"]),
+        #scripts=["util/gff_indexer.py"],
+        install_requires=[
+            "intervaltree",
+            "numpy",
+            "pandas"
+        ],
+        entry_points={
+            "console_scripts": [
+                "gffquant=gffquant.__main__:main",
+                "gffindex=gffquant.gff_indexer:main",
+                "collate_counts=gffquant.collate_counts:main"
+            ],
+        },
+        scripts=["nextflow/gffquant.nf"],
+        package_data={},
+        include_package_data=True,
+        data_files=[],
+    )
