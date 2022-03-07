b8e1dd51be710496b564260164dc60e50fac49ec
Christian Schudoma <cschu1981@gmail.com>
2022-03-07 16:58:44 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ with open(path.join(here, "DESCRIPTION.md"), encoding="utf-8") as description:
 				"collate_counts=gffquant.collate_counts:main",
 			],
 		},
-		scripts=["nextflow/gffquant.nf"],
+		# scripts=["nextflow/gffquant.nf"],
 		package_data={},
 		include_package_data=True,
 		data_files=[],
