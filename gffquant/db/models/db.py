3e9c8f15f4d6413dbbac103a9766e5aa4179a784
Christian Schudoma <cschu1981@gmail.com>
2022-05-12 13:25:24 +0200

    added index to annotatedsequence.seqid column

--- gffquant/db/models/db.py
+++ gffquant/db/models/db.py
@@ -10,7 +10,7 @@ class AnnotatedSequence(Base):
 	__tablename__ = "annotatedsequence"
 
 	id = Column(Integer, primary_key=True)
-	seqid = Column(String)
+	seqid = Column(String, index=True)
 	contig = Column(String)
 	start = Column(Integer)
 	end = Column(Integer)
