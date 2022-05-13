b8f7f67173dd804f38d181a01951926038ea83e9
Christian Schudoma <cschu1981@gmail.com>
2022-05-13 17:29:19 +0200

    modularised build_database

--- gffquant/bin/build_database.py
+++ gffquant/bin/build_database.py
@@ -1,7 +1,16 @@
 import argparse
 import csv
 import gzip
+import json
 import logging
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from ..db.models.meta import Base
+from gffquant.db import initialise_db
+from gffquant.db.models import db
+from gffquant.db.gff_dbm import GffDatabaseManager
 
 
 logging.basicConfig(
@@ -10,18 +19,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] %(message)s'
 )
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-
-
-from ..db.models.meta import Base
-from gffquant.db import initialise_db
-
-from gffquant.db.models import db
-
-from gffquant.db.gff_dbm import GffDatabaseManager
 
 
 def get_database(db_path):
@@ -40,28 +37,17 @@ def get_database(db_path):
 
 	return engine, db_session
 
-# # def initialise_db(engine):
-# 	Base.metadata.create_all(bind=engine)
+
+# def read_mapfile(fn):
+# 	with open(fn) as _in:
+# 		d = {}
+# 		for line in _in:
+# 			key, val = line.strip().split("\t")
+# 			d[key] = int(val)
+# 		return d
 
 
-
-def main():
-	ap = argparse.ArgumentParser()
-	ap.add_argument("db_path", type=str)
-	ap.add_argument("input_data", type=str)
-	ap.add_argument("--initialise_db", action="store_true")
-	ap.add_argument(
-        "--emapper_version",
-        type=str,
-        default="v2",
-        choices=("v1", "v2"),
-    )
-	args = ap.parse_args()
-
-	engine, db_session = get_database(args.db_path)
-
-	if args.initialise_db:
-		initialise_db(engine)
+def gather_category_and_feature_data(args, db_session=None):
 
 	cat_d = {}
 
@@ -69,7 +55,7 @@ def main():
 	gffdbm = GffDatabaseManager(args.input_data, "genes", emapper_version=args.emapper_version)
 
 	n = 0	
-	for n, (ref, region_annotation) in enumerate(gffdbm.iterate(bufsize=4000000000), start=1):
+	for n, (_, region_annotation) in enumerate(gffdbm.iterate(bufsize=4000000000), start=1):
 		for category, features in region_annotation[1:]:
 			cat_d.setdefault(category, set()).update(features)
 
@@ -79,8 +65,8 @@ def main():
 	code_map = {}
 	feature_offset = 0
 
-	with gzip.open(args.db_path + ".category.map.gz", "wt") as cat_out, gzip.open(args.db_path + ".feature.map.gz", "wt") as feat_out:
-		
+	# with gzip.open(args.db_path + ".category.map.gz", "wt") as cat_out, gzip.open(args.db_path + ".feature.map.gz", "wt") as feat_out:
+	with gzip.open(args.db_path + ".code_map.json.gz", "wt") as _map_out:
 		for category, features in sorted(cat_d.items()):
 			code_map[category] = {
 				"key": len(code_map),
@@ -90,28 +76,35 @@ def main():
 			}
 			feature_offset += len(features)
 	
-			print(category, code_map[category]["key"], sep="\t", file=cat_out)
-	
-			db_category = db.Category(id=code_map[category]["key"], name=category)
-			db_session.add(db_category)
-			db_session.commit()
+			if db_session is not None:
+				db_category = db.Category(id=code_map[category]["key"], name=category)
+				db_session.add(db_category)
+				db_session.commit()
 			
 			for feature, fid in code_map[category]["features"].items():
-	
-				print(feature, fid, sep="\t", file=feat_out)
-				
-				db_feature = db.Feature(id=fid, name=feature, category=db_category)
-				db_session.add(db_feature)
+				if db_session is not None:
+					db_feature = db.Feature(id=fid, name=feature, category=db_category)
+					db_session.add(db_feature)
 			
-			db_session.commit()
+			if db_session is not None:
+				db_session.commit()
 
-	
+		json.dump(code_map, _map_out)
+
+	return code_map, n
+
+
+def process_annotations(input_data, db_session, code_map, nseqs, emapper_version):
 	logging.info("Second pass: Encoding sequence annotations")
-	gffdbm = GffDatabaseManager(args.input_data, "genes", emapper_version=args.emapper_version)
+	gffdbm = GffDatabaseManager(input_data, "genes", emapper_version=emapper_version)
 	for i, (ref, region_annotation) in enumerate(gffdbm.iterate(bufsize=4000000000), start=1):
 		if i % 10000 == 0:
 			db_session.commit()
-			logging.info(f"Processed {i} entries. ({i/n * 100:.03f}%)")
+
+			if nseqs is not None:
+				logging.info(f"Processed {i} entries. ({i/nseqs * 100:.03f}%)")
+			else:
+				logging.info(f"Processed {i} entries.")
 
 		encoded = []
 		for category, features in region_annotation[1:]:
@@ -130,6 +123,41 @@ def main():
 		db_session.add(db_sequence)
 	db_session.commit()
 
+
+
+
+def main():
+	ap = argparse.ArgumentParser()
+	ap.add_argument("db_path", type=str)
+	ap.add_argument("input_data", type=str)
+	ap.add_argument("--initialise_db", action="store_true")
+	ap.add_argument("--code_map", type=str)
+	ap.add_argument("--nseqs", type=int)
+	ap.add_argument("--extract_map_only", action="store_true")
+	ap.add_argument(
+        "--emapper_version",
+        type=str,
+        default="v2",
+        choices=("v1", "v2"),
+    )
+	args = ap.parse_args()
+
+	engine, db_session = get_database(args.db_path) if not args.extract_map_only else (None, None)
+
+	if args.initialise_db and not args.extract_map_only:
+		initialise_db(engine)
+
+	nseqs = args.nseqs
+	if args.code_map:
+		with gzip.open(args.code_map, "rt") as _map_in:
+			code_map = json.load(_map_in)
+	else:
+		code_map, nseqs = gather_category_and_feature_data(args, db_session=db_session)
+
+	if args.extract_map_only:
+		return
+
+	process_annotations(args.input_data, db_session, code_map, nseqs, args.emapper_version)
 
 if __name__ == "__main__":
 	main()
