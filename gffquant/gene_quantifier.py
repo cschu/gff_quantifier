02f9c17fc05a121efb9e1dd9cfd0fea0324a13ee
Christian Schudoma <cschu1981@gmail.com>
2022-05-19 16:25:08 +0200

    added some logging

--- gffquant/gene_quantifier.py
+++ gffquant/gene_quantifier.py
@@ -87,6 +87,7 @@ class GeneQuantifier(FeatureQuantifier):
         return aln_count, 0, None
 
     def process_alignment_group(self, aln_group):
+        logging.info("Processing new alignment group %s (%s)", aln_group.qname.decode(), aln_group.n_align())
         ambig_counts = aln_group.get_ambig_align_counts()
         if any(ambig_counts) and self.require_ambig_bookkeeping:
             for aln in aln_group.get_alignments():
