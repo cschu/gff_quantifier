9454df5a8f27cb12c9b711881f580f4720cfdcfb
Christian Schudoma <cschu1981@gmail.com>
2022-06-15 17:00:21 +0200

    added depth/horizontal coverage to coverage output

--- gffquant/annotation/count_writer.py
+++ gffquant/annotation/count_writer.py
@@ -104,20 +104,154 @@ class CountWriter:
                 )
                 print(gene, *(f"{c:.5f}" for c in out_row), flush=True, sep="\t", file=gene_out)
 
+    # pylint: disable=R0914
     def write_coverage(self, db, coverage_counts):
-        d = {}
+        uniq_cov, combined_cov = {}, {}
+        uniq_depth, combined_depth = {}, {}
 
+        all_categories, all_features = set(), set()
         for cov_data in coverage_counts.values():
             n_features = sum(len(features) for _, features in cov_data["annotation"])
             for category, features in cov_data["annotation"]:
+                all_categories.add(category)
+                all_features.update(features)
                 for feature in features:
-                    d.setdefault(category, {}).setdefault(feature, []).append(
-                        cov_data["coverage"].mean() / n_features
+                    uniq_depth.setdefault(category, {}).setdefault(feature, []).append(
+                        cov_data["uniq_coverage"].mean() / n_features
+                    )
+                    combined_depth.setdefault(category, {}).setdefault(feature, []).append(
+                        cov_data["combined_coverage"].mean() / n_features
+                    )
+                    uniq_cov.setdefault(category, {}).setdefault(feature, []).append(
+                        (cov_data["uniq_coverage"] > 0).mean()
+                    )
+                    combined_cov.setdefault(category, {}).setdefault(feature, []).append(
+                        (cov_data["combined_coverage"] > 0).mean()
                     )
 
-        for category_id, features in d.items():
+        for category_id in sorted(all_categories):
             category = db.query_category(category_id).name
             with gzip.open(f"{self.out_prefix}.{category}.coverage.txt.gz", "wt") as feat_out:
-                for feature_id, feature_counts in features.items():
+                print(
+                    "category",
+                    "feature",
+                    "coverage_unique",
+                    "coverage_combined",
+                    "depth_unique",
+                    "depth_combined",
+                    sep="\t",
+                    file=feat_out,
+                )
+                for feature_id in sorted(all_features):
                     feature = db.query_feature(feature_id).name
-                    print(category, feature, np.mean(feature_counts), sep="\t", file=feat_out)
+                    uc, cc, ud, cd = [
+                        d.get(category_id, {}).get(feature_id)
+                        for d in (uniq_cov, combined_cov, uniq_depth, combined_depth)
+                    ]
+
+                    print(
+                        category,
+                        feature,
+                        np.mean(uc) if uc is not None else "NA",
+                        np.mean(cc) if cc is not None else "NA",
+                        np.mean(ud) if ud is not None else "NA",
+                        np.mean(cd) if cd is not None else "NA",
+                        sep="\t",
+                        file=feat_out,
+                    )
+
+# pylint: disable=W0105
+"""
+domain_cov = dict()
+        for pos in set(ref_coverage).union(ambig_ref_coverage):
+            # features_at_position = pos_feature_dict.get(pos, set())
+            for domtype in pos_feature_dict.get(pos, set()):
+                domain_cov.setdefault(domtype, dict()).update(
+                    {
+                        "depth_uniq": list(),
+                        "depth_ambig": list(),
+                        "cov_uniq": list(),
+                        "cov_ambig": list(),
+                    }
+                )
+
+                uniq_cov = ref_coverage.get(pos, Counter())
+                ambig_cov = ambig_ref_coverage.get(pos, Counter())
+
+                if uniq_cov:
+                    domain_cov[domtype]["depth_uniq"].append(
+                        sum(uniq_cov.values()) / len(uniq_cov.values())
+                    )
+                    domain_cov[domtype]["cov_uniq"].append(
+                        sum(1 for v in uniq_cov.values() if v) / len(uniq_cov.values())
+                    )
+                if ambig_cov:
+                    domain_cov[domtype]["depth_ambig"].append(
+                        sum(ambig_cov.values()) / len(ambig_cov.values())
+                    )
+                    domain_cov[domtype]["cov_ambig"].append(
+                        sum(1 for v in ambig_cov.values() if v) / len(ambig_cov.values())
+                    )
+
+        with gzip.open(self.out_prefix + ".covsum.txt.gz", "wt") as cov_out:
+            print("#domain", "depth_unique", "depth_combined", "coverage_unique", "coverage_combined", sep="\t", file=cov_out,)
+            for domtype, counts in sorted(domain_cov.items()):
+
+                depth_uniq, depth_ambig = [
+                    c for c in counts.get("depth_uniq", list()) if c is not None
+                ], [c for c in counts.get("depth_ambig", list()) if c is not None]
+
+                depth_uniq_ = (sum(depth_uniq) / len(depth_uniq)) if depth_uniq else 0
+                print(domtype, "UNIQ", depth_uniq, sum(depth_uniq), len(depth_uniq), "=", depth_uniq_,)
+
+                depth_ambig_ = (
+                    (sum(depth_ambig) / len(depth_ambig)) if depth_ambig else 0
+                )
+                print(domtype, "AMBIG", depth_ambig, sum(depth_ambig), len(depth_ambig), "=", depth_ambig_,)
+
+                if depth_ambig_ < depth_uniq_:
+                    raise ValueError(
+                        f"{domtype}: depth_uniq_ + depth_ambig_ {depth_ambig_:.5f} is smaller than uniq depth {depth_uniq_:.5f}."
+                    )
+
+                cov_uniq, cov_ambig = [
+                    c for c in counts.get("cov_uniq", list()) if c is not None
+                ], [c for c in counts.get("cov_ambig", list()) if c is not None]
+
+                cov_uniq_ = (sum(cov_uniq) / len(cov_uniq)) if cov_uniq else 0
+                print(
+                    domtype,
+                    "UNIQ",
+                    cov_uniq,
+                    sum(cov_uniq),
+                    len(cov_uniq),
+                    "=",
+                    cov_uniq_,
+                )
+
+                cov_ambig_ = (sum(cov_ambig) / len(cov_ambig)) if cov_ambig else 0
+                print(
+                    domtype,
+                    "AMBIG",
+                    cov_ambig,
+                    sum(cov_ambig),
+                    len(cov_ambig),
+                    "=",
+                    cov_ambig_,
+                )
+
+                if cov_ambig_ < cov_uniq_:
+                    raise ValueError(
+                        f"{domtype}: cov_uniq_ + cov_ambig_ {cov_ambig_:.5f} is smaller than uniq cov {cov_uniq_:.5f}."
+                    )
+
+                # print(domtype, f"{depth_uniq_:.5f}", f"{depth_ambig_:.5f}", sep="\t", file=cov_out)
+                print(
+                    domtype,
+                    *(
+                        f"{val:.5f}"
+                        for val in (depth_uniq_, depth_ambig_, cov_uniq_, cov_ambig_)
+                    ),
+                    sep="\t",
+                    file=cov_out,
+                )"""
--- gffquant/counters/coverage_counter.py
+++ gffquant/counters/coverage_counter.py
@@ -11,12 +11,18 @@ class CoverageCounter(dict):
 
     # pylint: disable=R0913
     def update_coverage(self, rid, start, end, uniq_counts, ambig_counts, annotation):
+        region_length = end - start + 1
         cov = self.setdefault(
             (rid, start, end),
             {
-                "coverage": np.zeros(end - start + 1),
+                "uniq_coverage": np.zeros(region_length),
+                "combined_coverage": np.zeros(region_length),
                 "annotation": annotation
             }
         )
-        for rstart, rend, rcount in chain(*uniq_counts, *ambig_counts):
-            cov["coverage"][rstart:rend] += rcount
+
+        for rstart, rend, rcount in chain(*uniq_counts):
+            cov["uniq_coverage"][rstart:rend] += rcount
+            cov["combined_coverage"][rstart:rend] += rcount
+        for rstart, rend, rcount in chain(*ambig_counts):
+            cov["combined_coverage"][rstart:rend] += rcount
