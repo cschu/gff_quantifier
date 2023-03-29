e278688c7c37130b5854522325135e94d6950eaf
Christian Schudoma <cschu1981@gmail.com>
2023-03-29 18:13:48 +0200

    tool: db-based profile collation

---
+++ gffquant/bin/db_collate.py
@@ -0,0 +1,88 @@
+import argparse
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from ..db import initialise_db
+from ..db.models import db
+from ..db.models.meta import Base
+
+
+def get_database(db_path):
+    engine = create_engine(f"sqlite:///{db_path}")
+
+    db_session = scoped_session(
+        sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            enable_baked_queries=True,
+            bind=engine
+        )
+    )
+
+    Base.query = db_session.query_property()
+
+    return engine, db_session
+
+
+def main():
+	ap = argparse.ArgumentParser()
+	ap.add_argument("--input_dir", "-i", type=str, default=".")
+	ap.add_argument("--output_dir", "-o", type=str, default="collated")
+	ap.add_argument("--column", "-c", type=str)
+	ap.add_argument("--dbpath", type=str, default="observations.sqlite3")
+	
+	args = ap.parse_args()
+
+	dirpath, _, files = next(os.walk(args.input_dir))
+
+	engine, db_session = get_database(args.db_path) if not args.extract_map_only else (None, None)
+	initialise_db(engine)
+
+
+	for f in files:
+		fname = os.path.basename(f).replace(".txt.gz", "")
+		*sample, category = fname.split(".")
+		sample = ".".join(sample)
+
+		db_category = db_session.query(db.Category.name == category)
+		if db_category is None:
+			db_category = db.Category(name=category)
+			db_session.add(db_category)
+			db_session.commit()
+
+		db_sample = db_session.query(db.Sample.name == sample).one_or_none()
+		if db_sample is None:
+			db_sample = db.Sample(name=sample)
+			db_session.add(db_sample)
+			db_session.commit()
+		
+
+		with open(os.path.join(dirpath, f), "rt") as _in:
+			for row in csv.DictReader(_in, delimiter="\t"):
+				if not "unannotated" in row:
+					db_feature = db_session.query(db.Feature.name == row["feature"]).one_or_none()
+					if db_feature is None:
+						db_feature = db.Feature(name=row["feature"], category=category)
+						db_session.add(db_feature)
+						db_session.commit()
+					db_observation = db.Observation(
+						metric=args.column,
+						value=row[args.column],
+						category_id=db_category.id,
+						sample_id=db_sample.id
+					)
+					db_session.add(db_observation)
+					db_session.commit()
+
+
+
+
+
+
+	
+	
+if __name__ == "__main__":
+	main()
