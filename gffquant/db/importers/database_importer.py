1fa46beb8172309f060fe08126f6656eafa753e7
Christian Schudoma <christian.schudoma@embl.de>
2023-10-04 23:07:51 +0200

    combine category and annotation parsing - fix

--- gffquant/db/importers/database_importer.py
+++ gffquant/db/importers/database_importer.py
@@ -92,18 +92,22 @@ class GqDatabaseImporter(ABC):
                 logger.info("    Loaded %s entries.", str(i))
 
 
-            if self.db_session:
-                for cat_name, cat_id in category_map.items():
-                    db_category = db.Category(id=cat_id, name=cat_name)
-                    if self.db_session is not None:
-                        self.db_session.add(db_category)
+            for cat_name, cat_id in category_map.items():
+                db_category = db.Category(id=cat_id, name=cat_name)
+                if self.db_session is not None:
+                    self.db_session.add(db_category)
+                else:
+                    self.categories[db_category.id] = db_category
 
-                for (cat_id, feat_name), feat_id in feature_map.items():
-                    db_feature = db.Feature(id=feat_id, name=feat_name, category_id=cat_id)
-                    if self.db_session is not None:
-                        self.db_session.add(db_feature)                
+            for (cat_id, feat_name), feat_id in feature_map.items():
+                db_feature = db.Feature(id=feat_id, name=feat_name, category_id=cat_id)
+                if self.db_session is not None:
+                    self.db_session.add(db_feature)
+                else:
+                    self.features[db_feature.id] = db_feature
 
-                    self.db_session.commit()
+            if self.db_session is not None:
+                self.db_session.commit()
 
             
             
