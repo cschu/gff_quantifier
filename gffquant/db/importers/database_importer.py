cb8dae5623fc9f1a9e21814d2a330101fcb94971
Christian Schudoma <christian.schudoma@embl.de>
2023-10-04 22:54:31 +0200

    combine category and annotation parsing - fix

--- gffquant/db/importers/database_importer.py
+++ gffquant/db/importers/database_importer.py
@@ -91,21 +91,19 @@ class GqDatabaseImporter(ABC):
             else:
                 logger.info("    Loaded %s entries.", str(i))
 
-            for cat_name, cat_id in category_map.items():
-                db_category = db.Category(id=cat_id, name=cat_name)
-                self.categories[db_category.id] = db_category
-                if self.db_session is not None:
-                    self.db_session.add(db_category)
-
-            for (cat_id, feat_name), feat_id in feature_map.items():
-                db_feature = db.Feature(id=feat_id, name=feat_name, category_id=cat_id)
-                if self.db_session is not None:
-                    self.db_session.add(db_feature)                
-
-            self.db_session.commit()
 
             if self.db_session:
-                self.db_session.commit()
+                for cat_name, cat_id in category_map.items():
+                    db_category = db.Category(id=cat_id, name=cat_name)
+                    if self.db_session is not None:
+                        self.db_session.add(db_category)
+
+                for (cat_id, feat_name), feat_id in feature_map.items():
+                    db_feature = db.Feature(id=feat_id, name=feat_name, category_id=cat_id)
+                    if self.db_session is not None:
+                        self.db_session.add(db_feature)                
+
+                    self.db_session.commit()
 
             
             
