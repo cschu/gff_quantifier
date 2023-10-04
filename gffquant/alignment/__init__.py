2f80e70bb91e9046df3fd3a5db8b3561b41e2ade
Christian Schudoma <christian.schudoma@embl.de>
2023-10-04 20:11:45 +0200

    import order

--- gffquant/alignment/__init__.py
+++ gffquant/alignment/__init__.py
@@ -3,5 +3,5 @@
 """ module docstring """
 
 from .aln_group import AlignmentGroup
-from .pysam_alignment_processor import AlignmentProcessor
 from .bamreader import SamFlags
+from .pysam_alignment_processor import AlignmentProcessor
