dce98fa4a95a3353e500119552ef391b4fdc1e24
Christian Schudoma <christian.schudoma@embl.de>
2024-06-03 17:43:18 +0200

    added polars as requirement

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ with open(path.join(here, "DESCRIPTION.md"), encoding="utf-8") as description:
 			"pandas",
 			"pysam",
 			"sqlalchemy",
+			"polars",
 		],
 		entry_points={
 			"console_scripts": [
