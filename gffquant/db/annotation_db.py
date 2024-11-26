a9d38acbb1556e7967368bf6c36f7a65f102bca7
Christian Schudoma <christian.schudoma@embl.de>
2024-11-26 13:17:13 +0100

    refactor annotation_db

--- gffquant/db/annotation_db.py
+++ gffquant/db/annotation_db.py
@@ -5,6 +5,7 @@
 import logging
 
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from functools import lru_cache
 
 from intervaltree import IntervalTree
@@ -15,6 +16,23 @@ from .importers import GqDatabaseImporter
 
 
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class OverlapResultHeader:
+    has_target: bool
+
+
+@dataclass
+class OverlapTarget:
+    start: int = None
+    end: int = None
+    seq_feature: db.AnnotatedSequence = None
+    cov_start: int = None
+    cov_end: int = None
+
+    def has_annotation(self):
+        return self.seq_feature is not None and bool(self.seq_feature.annotation_str)
 
 
 class AnnotationDatabaseManager(ABC):
@@ -31,6 +49,10 @@ class AnnotationDatabaseManager(ABC):
 
     @abstractmethod
     def query_sequence_internal(self, seqid, start=None, end=None):
+        ...
+
+    @abstractmethod
+    def get_categories(self):
         ...
 
     def query_sequence(self, seqid, start=None, end=None):
@@ -67,24 +89,28 @@ class AnnotationDatabaseManager(ABC):
         # pylint: disable=W2301
         ...
 
-    def get_interval_tree_overlaps(self, seqid, qstart, qend):
+    def get_interval_tree_overlaps(self, seqid, qstart, qend, with_coverage=False):
         db_sequences = {
             (seq.start, seq.end + 1): seq
             for seq in self.get_db_sequence(seqid)
         }
 
-        yield bool(db_sequences), None, None, None
+        # yield bool(db_sequences), None, None, None
+        yield OverlapResultHeader(bool(db_sequences))
 
         itree = IntervalTree.from_tuples(db_sequences)
 
         for interval in itree[qstart:qend + 1]:
-            yield None, interval.begin, interval.end - 1, db_sequences.get((interval.begin, interval.end))
+            # yield None, interval.begin, interval.end - 1, db_sequences.get((interval.begin, interval.end))
+            cov_start, cov_end = (max(qstart, interval.begin), min(qend, interval.end - 1)) if with_coverage else (None, None)
+            yield OverlapTarget(interval.begin, interval.end - 1, db_sequences.get((interval.begin, interval.end)), cov_start, cov_end)
 
     def get_interval_overlaps(self, seqid, qstart, qend):
         """ return all intervals overlapping the query read """
         db_sequences = self.get_db_sequence(seqid)
 
-        yield bool(db_sequences), None, None, None
+        # yield bool(db_sequences), None, None, None
+        yield OverlapResultHeader(bool(db_sequences))
 
         for seq in db_sequences:
             # we're assuming
@@ -93,7 +119,8 @@ class AnnotationDatabaseManager(ABC):
             if qend < seq.start - 1 or seq.end - 1 < qstart:
                 continue
 
-            yield None, seq.start, seq.end, None
+            # yield None, seq.start, seq.end, None
+            yield OverlapTarget(seq.start, seq.end, None)
 
             # interval = seq.start, seq.end
             # sstart, send = seq.start - 1, seq.end
@@ -120,7 +147,7 @@ class AnnotationDatabaseManager(ABC):
             return (max(qstart, sstart), send)
         raise ValueError(f"Cannot happen. interval=({sstart}, {send}) vs ({qstart}, {qend})")
 
-    def get_overlaps(self, seqid, start, end, domain_mode=False):
+    def get_overlaps(self, seqid, start, end, domain_mode=False, with_coverage=False):
 
         # def calc_covered_fraction(start, end, interval):
         #     if interval.begin <= start <= end <= interval.end:
@@ -134,7 +161,7 @@ class AnnotationDatabaseManager(ABC):
         if domain_mode:
             return self.get_interval_overlaps(seqid, start, end)
 
-        return self.get_interval_tree_overlaps(seqid, start, end)
+        return self.get_interval_tree_overlaps(seqid, start, end, with_coverage=with_coverage)
 
         # return (
         #     (interval.begin + 1, interval.end)
@@ -180,9 +207,20 @@ class SQL_ADM(AnnotationDatabaseManager):
     def query_category(self, category_id):
         return self.db_session.query(db.Category).filter(db.Category.id == category_id).one_or_none()
 
+    def get_categories(self):
+        return self.db_session.query(db.Category).all()
+
+    def get_features(self, category=None):
+        return self.db_session.query(db.Feature).filter(db.Feature.category_id == category).all()
+    
     @lru_cache(maxsize=10000)
-    def get_db_sequence(self, seqid):
-        return self.db_session.query(db.AnnotatedSequence).filter(db.AnnotatedSequence.seqid == seqid).all()
+    def get_db_sequence(self, seqid, start=None, end=None):
+        seqs = self.db_session.query(db.AnnotatedSequence).filter(db.AnnotatedSequence.seqid == seqid).all()
+        return [
+            seq
+            for seq in seqs
+            if (start is None or start == seq.start) and (end is None or end == end.start)
+        ]
 
 
 class Dict_ADM(AnnotationDatabaseManager):
@@ -192,7 +230,11 @@ class Dict_ADM(AnnotationDatabaseManager):
 
     def query_sequence_internal(self, seqid, start=None, end=None):
         if start is not None and end is not None:
-            seqs = [seq for seq in self.db.annotations.get(seqid, [None]) if seq.start == start and seq.end == end]
+            seqs = [
+                seq
+                for seq in self.db.annotations.get(seqid, [])
+                if seq.start == start and seq.end == end
+            ] + [None]
             return seqs[0]
         return self.db.annotations.get(seqid, [None])[0]
 
@@ -212,6 +254,29 @@ class Dict_ADM(AnnotationDatabaseManager):
     def query_category(self, category_id):
         return self.db.categories.get(int(category_id))
 
+    def get_categories(self):
+        yield from self.db.categories.values()
+
+    def get_features(self, category=None):
+        for feat in self.db.features.values():
+            if category is None or feat.category_id == category:
+                yield feat
+    
     @lru_cache(maxsize=10000)
-    def get_db_sequence(self, seqid):
-        return self.db.annotations.get(seqid, [])
+    def get_db_sequence(self, seqid, start=None, end=None):
+        seqs = self.db.annotations.get(seqid, [])
+        return [
+            seq
+            for seq in seqs
+            if (start is None or start == seq.start) and (end is None or end == seq.end)
+        ]
+
+    def dump(self, prefix):
+        # pylint: disable=C0415
+        import pickle
+        with open(f"{prefix}.annotations.dat", "wb") as _out:
+            pickle.dump(self.db.annotations, _out)
+        with open(f"{prefix}.features.dat", "wb") as _out:
+            pickle.dump(self.db.features, _out)
+        with open(f"{prefix}.categories.dat", "wb") as _out:
+            pickle.dump(self.db.categories, _out)
