5ed8cfc95c7b76a1e58a1bfc20d264a2bff78002
Christian Schudoma <christian.schudoma@embl.de>
2024-12-19 22:15:37 +0100

    fixed import

--- gffquant/annotation/__init__.py
+++ gffquant/annotation/__init__.py
@@ -2,5 +2,7 @@
 
 """ module docstring """
 
-from .count_annotator import GeneCountAnnotator, RegionCountAnnotator
+# from .count_annotator import GeneCountAnnotator, RegionCountAnnotator
+from .count_annotator import RegionCountAnnotator
 from .count_writer import CountWriter
+from .genecount_annotator import GeneCountAnnotator
