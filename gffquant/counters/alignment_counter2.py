fbb6a0edb6b6a3e928811d36b9c3e450e18032e2
Christian Schudoma <christian.schudoma@embl.de>
2024-12-21 21:21:53 +0100

    dump seqcounters for debugging

--- gffquant/counters/alignment_counter2.py
+++ gffquant/counters/alignment_counter2.py
@@ -38,9 +38,9 @@ class AlignmentCounter:
     def dump(self, prefix, refmgr):
         import gzip
         with gzip.open(f"{prefix}.{self.__class__.__name__}.txt.gz", "wt") as _out:
-            for key in self.index.keys():
+            for key, key_index in self.index.items():
                 ref, reflen = refmgr.get(key[0] if isinstance(key, tuple) else key)
-                print(key, ref, reflen, self.counts[key], sep="\t", file=_out)
+                print(key, ref, reflen, self.counts[key_index], sep="\t", file=_out)
             # for k, v in self.items():
             # ref, reflen = refmgr.get(k[0] if isinstance(k, tuple) else k)
             # print(k, ref, reflen, v, sep="\t", file=_out)
