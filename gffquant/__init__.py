5121120f6c513aeb466d4500ac90149c728c65d4
Christian Schudoma <christian.schudoma@embl.de>
2025-02-12 11:32:55 +0100

    version -> 2.19.0, move referencehit to own module

--- gffquant/__init__.py
+++ gffquant/__init__.py
@@ -5,7 +5,7 @@
 from enum import Enum, auto, unique
 
 
-__version__ = "2.18.2"
+__version__ = "2.19.0"
 __tool__ = "gffquant"
 
 
--- gffquant/profilers/feature_quantifier.py
+++ gffquant/profilers/feature_quantifier.py
@@ -13,6 +13,7 @@ from collections import Counter
 from dataclasses import dataclass, asdict
 
 from .panda_coverage_profiler import PandaCoverageProfiler
+from .reference_hit import ReferenceHit
 from ..alignment import AlignmentGroup, AlignmentProcessor, SamFlags
 from ..annotation import GeneCountAnnotator, RegionCountAnnotator, CountWriter
 from ..counters import CountManager
@@ -22,39 +23,6 @@ from .. import __tool__, DistributionMode, RunMode
 
 
 logger = logging.getLogger(__name__)
-
-
-@dataclass(slots=True)
-class ReferenceHit:
-    rid: int = None
-    start: int = None
-    end: int = None
-    rev_strand: bool = None
-    cov_start: int = None
-    cov_end: int = None
-    has_annotation: bool = None
-    n_aln: int = None
-    is_ambiguous: bool = None
-    library_mod: int = None
-    mate_id: int = None
-
-    def __hash__(self):
-        return hash(tuple(asdict(self).values()))
-
-    def __eq__(self, other):
-        return all(
-            item[0][1] == item[1][1]
-            for item in zip(
-                sorted(asdict(self).items()),
-                sorted(asdict(other).items())
-            )
-        )
-
-    def __str__(self):
-        return "\t".join(map(str, asdict(self).values()))
-
-    def __repr__(self):
-        return str(self)
 
 
 class FeatureQuantifier(ABC):
